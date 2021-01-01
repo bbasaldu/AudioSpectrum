5561f38fb3a72bcd75419c97656d6ae44f3d1195
bbasaldu <50085901+bbasaldu@users.noreply.github.com>
2020-12-31 19:51:11 -0800

    Add files via upload

--- Audio.js
+++ Audio.js
@@ -4,7 +4,7 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API
 
 //no copyright
-const song = "TrapTitans_Pinnacle.mp3";
+const defaultSong = "TrapTitans_Pinnacle.mp3";
 const pow = 3;
 const radialMargin = 25;
 const maxHz = 44100/2; //We can only get data on half the sample rate, max hz our data gives us
@@ -12,11 +12,6 @@ const maxHumanHz = 20000; //upper limit of human hearing
 const margin = 0;
 const dim = {w: window.innerWidth-margin, h: window.innerHeight-margin};
 
-//console.log(dim)
-let analyser;
-let bufferLength;
-let audio;
-let dataArray;
 
 var width = 1000;//dim.w;//1000
 var height = 650;//dim.h;//650
@@ -25,10 +20,13 @@ window.mobileCheck = function() {
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 };
+//mobile version -> scale up a bit
+//desktop version -> add some auto centering and drag n drop ui.
 if(window.mobileCheck()){
     height = height * 2;
 }
 else {
+    //showDropZone();
     window.onresize = function (){resize(window.innerWidth/2, window.innerHeight/2)};
 }
 d3.select('#name').attr('class', 'h4')
@@ -45,7 +43,7 @@ var svg = div.append('svg')
     //.style('transform', 'translate(500, 500)')
     //.style('display', 'block')
     //.style('margin', 'auto')
-    .on('load', () => resize(dim.w/2, dim.h/2))
+    .on('load', () => {resize(dim.w/2, dim.h/2); if(!window.mobileCheck()) showDropZone()})
 //https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
 
 
@@ -181,7 +179,8 @@ function showHearingRange(){
         .append('textPath')
         .attr('startOffset', '25%')
         .attr('href', '#beyondHearingRange')
-        .text('(20,000Hz <-> 20Hz)')
+        .text('(Max/Min: 22.05kHz/0Hz)')
+    
     
     rangeGroup.append('line')
         .attr('id', 'tick1')
@@ -230,16 +229,15 @@ function showHearingRange(){
         .text('20Hz')
     
     rangeGroup.append('text')
-        
         .attr('fill', '#fff')
-        .attr('dy', 16)
+        .attr('dy', 12)
         .attr('font-size', tickFont)
         //.attr('text-anchor')
         .append('textPath')
         .attr('startOffset', '70.75%')
         .attr('href', '#hearingRange')
         .text('Human Hearing Range')
-
+    
         rangeGroup.selectAll('text')
             .attr('opacity',0)
             .transition()
@@ -534,8 +532,48 @@ function btnClick(btn){
     else btn.attr('stroke', '#fff').attr('clicked', 'f')
 }
 
-var btnGroup = svg.append('g')
+let song = null;
+let analyser;
+let bufferLength;
+let audio;
+let dataArray;
+let AudioContext;
+let source = null;
+let audioCtx = null;
+function initAudioCtx(){
+    AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioCtx = new AudioContext()
+    analyser = audioCtx.createAnalyser();
+    analyser.fftSize = bins * 2;//2048;
+    analyser.smooothingTimeConstant = 0;//maybe 0.8
+    bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+    analyser.connect(audioCtx.destination)
+}
+initAudioCtx();
+//requestAnimationFrame(tick);
+//for drop event
+function loadSong(song){
+    if(source!=null){
+        if(!audio.paused) btnClick(d3.select('#playBtn'))
+        audio.currentTime = audio.duration//end song
+        //audio.pause()
+        source.disconnect(analyser)
+        //songChanged = true;
+    }
+    
+    
+    audio = new Audio(song)
+    if(source == null) requestAnimationFrame(tick);
+    source = audioCtx.createMediaElementSource(audio)
+    source.connect(analyser)
+    
+    //audio.play()
+    //songChanged = false;
+    //requestAnimationFrame(tick)    
+}
 
+var btnGroup = svg.append('g')
 var playBtn = btnGroup.append('circle')
     .attr('id', 'playBtn')
     .attr('cx', midx)
@@ -544,33 +582,46 @@ var playBtn = btnGroup.append('circle')
     .attr('stroke', '#fff')
     .on('click', function() {
         btnClick(d3.select(this))
-        if(typeof audio == 'undefined'){
-            const AudioContext = window.AudioContext || window.webkitAudioContext;
-            const audioCtx = new AudioContext() 
-            //const gainNode = audioCtx.createGain()
-            //gainNode.gain.value = 1.5
-            analyser = audioCtx.createAnalyser();
-            //minimum 32
-            analyser.fftSize = bins * 2;//2048;
-            analyser.smooothingTimeConstant = 0;
-            bufferLength = analyser.frequencyBinCount;
-            dataArray = new Uint8Array(bufferLength);
-            //analyser.getByteFrequencyData(dataArray);
-
-
-            audio = new Audio(song)
-            const source = audioCtx.createMediaElementSource(audio)
-            //source.connect(gainNode)
-            //gainNode.connect(analyser)
-            source.connect(analyser)
-            analyser.connect(audioCtx.destination)
-            //console.log(dataArray)
-            //console.log(audioCtx.sampleRate)
-            audio.play()
-            //console.log('here')
-            requestAnimationFrame(tick)   
+//        if(audioCtx == null){
+//            //initAudioCtx();
+//            //loadSong(defaultSong);
+//            requestAnimationFrame(tick)
+//            //audio.play();
+//        }
+        if(song == null){
+            song = defaultSong;
+            loadSong(song)
         }
-        else play();
+        play();
+        
+        
+//        if(typeof audio == 'undefined'){
+//            const AudioContext = window.AudioContext || window.webkitAudioContext;
+//            const audioCtx = new AudioContext() 
+//            //const gainNode = audioCtx.createGain()
+//            //gainNode.gain.value = 1.5
+//            analyser = audioCtx.createAnalyser();
+//            //minimum 32
+//            analyser.fftSize = bins * 2;//2048;
+//            analyser.smooothingTimeConstant = 0;
+//            bufferLength = analyser.frequencyBinCount;
+//            dataArray = new Uint8Array(bufferLength);
+//            //analyser.getByteFrequencyData(dataArray);
+//
+//
+//            audio = new Audio(defaultSong)
+//            const source = audioCtx.createMediaElementSource(audio)
+//            //source.connect(gainNode)
+//            //gainNode.connect(analyser)
+//            source.connect(analyser)
+//            analyser.connect(audioCtx.destination)
+//            //console.log(dataArray)
+//            //console.log(audioCtx.sampleRate)
+//            audio.play()
+//            //console.log('here')
+//            requestAnimationFrame(tick)   
+//        }
+//        else play();
     })
 
 var playBtnText = btnGroup.append('text')
@@ -652,9 +703,110 @@ btnGroup.selectAll('text')
     .attr('pointer-events', 'none')
 
 d3.select('#name').attr('pointer-events', 'none')
+let rw;
+function showDropZone(){
+    
+    window.addEventListener("dragover",function(e){
+        //d3.select('#dropZone').attr('stroke', '#fff')
+      e = e || event;
+      e.preventDefault();
+        
+    },false);
+
+    window.addEventListener("drop",function(e){
+      e = e || event;
+      e.preventDefault();
+    },false);
+    
+    
+    rw = (midx+50+btnR)-(midx-50-btnR)
+    var rh = btnR*1.5
+    
+    btnGroup.append('rect')
+        .attr('id', 'dropZone')
+        .attr('x', (midx-50)-btnR)
+        .attr('y', 125+btnR+10)
+        .attr('width', rw)
+        .attr('rx', 10)
+        .attr('ry', 10)
+        .attr('height', rh)
+        .attr('stroke', '#fff')
+        .on('drop', dropHandler)
+        .on('dragover', dragOverHandler)
+        .on('dragleave', () => d3.select('#dropZone').attr('stroke', '#fff'))
+    
+    btnGroup.append('text')
+        .attr('pointer-events', 'none')
+        .attr('font-size', '12px')
+        .attr('x', (midx-50)-btnR + (rw/2))
+        .attr('fill', '#fff')
+        .attr('y', (125+btnR+2.5) + (rh/2))
+        .attr('text-anchor', 'middle')
+        .text('Drag and Drop Audio File Here')
+    
+    //d3.select('#fileUpload').on('change', () => console.log('change'))
+    btnGroup.append('text')
+        .attr('pointer-events', 'auto')
+        .attr('cursor', 'pointer')
+        .attr('font-size', '12px')
+        .attr('x', (midx-50)-btnR + (rw/2))
+        .attr('fill', '#fff')
+        .attr('y', (125+btnR+22.5) + (rh/2))
+        .attr('text-anchor', 'middle')
+        .style('text-decoration', 'underline')
+        .text('Or Upload')
+        .on('click', fileUpload)
+    
+    
+    
+    btnGroup.append('text')
+        .attr('id', 'songTitle')
+        .attr('pointer-events', 'none')
+        .attr('font-size', '12px')
+        .attr('x', (midx-50)-btnR + (rw/2))
+        .attr('fill', '#fff')
+        .attr('y', (125+btnR+55) + (rh/2))
+        .attr('text-anchor', 'middle')
+        .text('')
+    d3.selectAll('text').attr('font-family', btnFF)
+    
+}
+
+function fileChange(){
+    var file = d3.select('#fileUpload').node().files
+}
+
+function fileUpload(){
+    let input = d3.select('#fileUpload')
+    input.node().click()
+    input.on('change', () => {
+        var file = input.node().files[0]
+        if(file != null){
+            song = URL.createObjectURL(file)
+            loadSong(song);
+            d3.select('#songTitle').text(file.name).call(wrap, rw)
+            d3.select('#dropZone').attr('stroke', '#fff')
+        }
+    })
+}
+
+function dropHandler(ev) {
+    ev.preventDefault();
+    var file = ev.dataTransfer.files[0]
+    song = URL.createObjectURL(file)
+    loadSong(song);
+    d3.select('#songTitle').text(file.name).call(wrap, rw)
+    d3.select('#dropZone').attr('stroke', '#fff')
+}
+function dragOverHandler(ev) {
+    d3.select('#dropZone').attr('stroke', btnColor)
+    ev.preventDefault();
+}
+
+//showDropZone();
 function play(){
     if(audio.paused){
-        if(audio.ended) requestAnimationFrame(tick);
+        endBool = true;
         audio.play();
     }
     else audio.pause()
@@ -693,22 +845,28 @@ function wrap(text, width) {
 }
 let start;
 let elapsed;
-
+let endBool = true;
 function tick(){
     
     analyser.getByteFrequencyData(dataArray);
     
     Chart.attr('d', (d, index) => d.arc.outerRadius(yScale(Math.pow(dataArray[index], pow)))())
         .attr('fill', (d, index) => d3.interpolatePuRd(colorScale(Math.pow(dataArray[index], pow))))
-    
-    if(!audio.ended) requestAnimationFrame(tick)
-    else {
-        analyser.smooothingTimeConstant = 1.0;
-        requestAnimationFrame(endTick)
+    //console.log(audio.ended)
+    requestAnimationFrame(tick)
+    if(audio.ended && endBool){
+        btnClick(d3.select('#playBtn'));
+        endBool = false;
     }
+//    if(!audio.ended) requestAnimationFrame(tick)
+//    else {
+//        analyser.smooothingTimeConstant = 1.0;
+//        requestAnimationFrame(endTick)
+//    }
 }
 function endTick(){
     //analyser.smooothingTimeConstant = 1.0;
+    console.log('here')
     analyser.getByteFrequencyData(dataArray)
     Chart.attr('d', (d, index) => d.arc.outerRadius(yScale(0))())
         .attr('fill', (d, index) => d3.interpolatePuRd(colorScale(Math.pow(dataArray[index], pow))))
